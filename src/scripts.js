6dcf684dcd4e8435ec49f397088aeb70c3b06dab
Omar Elsharkawy <omarx10050@gmail.com>
2026-06-23 19:29:28 +0300

    getplayerguess function

--- src/scripts.js
+++ src/scripts.js
@@ -10,46 +10,80 @@ function generateRandomNumber(){
 //For testing purposes only
 // console.log("Generated Number:", generatedNumber);
 
-function getValidGuess() {
+// function getValidGuess() {
 
-    while (true) {
-        let playerInput = prompt(
-            "You can't guess the integer number between 1 and 100 i'm thinking about!", " "
-        );
+//     while (true) {
+//         let playerInput = prompt(
+//             "You can't guess the integer number between 1 and 100 i'm thinking about!", " "
+//         );
 
-        if (playerInput === null) {
-            return null;
-        }
-        if (/^\d+$/.test(playerInput.trim())) {
-            let playerGuess = parseInt(playerInput, 10);
+//         if (playerInput === null) {
+//             return null;
+//         }
+//         if (/^\d+$/.test(playerInput.trim())) {
+//             let playerGuess = parseInt(playerInput, 10);
 
-            if (playerGuess >= 1 && playerGuess <= 100) {
-                return playerGuess;
-            } else {
-                console.log("...Do you even know, what in tarnation is an integer?!!");
-            }
-        }
-    }
+//             if (playerGuess >= 1 && playerGuess <= 100) {
+//                 return playerGuess;
+//             } else {
+//                 console.log("...Do you even know, what in tarnation is an integer?!!");
+//             }
+//         }
+//     }
+// }
+
+async function getPlayerGuess() {
+    // const generatedNumber = generateRandomNumber();
+    // console.log("test");
+    // console.log(generatedNumber);
+
+    while(true){
+    let playerGuess = prompt("Guess a random number between 1 and 100");
+
+       await wait()    // checkGuess(generatedNumber, playerGuess);
+
+    // else if  (/^\d+$/.test(playerGuess.trim())) {
+    //     console.log("Enter valid character");
+    // }
+    if(playerGuess === null || playerGuess.trim() === ""){
+        console.log("Write something!!!!");
+        continue;
+    } 
+    const number = parseInt(playerGuess,10);
+    if(isNaN(number) || playerGuess.trim() !== number.toString()) {
+        console.log("Please enter a valid integer.");
+        continue;
+     } 
+     else{
+        console.log(number);
+        return number;
+     }
+
+//     else if (number > 100 || number < 1){
+//         console.log("Choose a number between 1 and 100 not higher or lower");
+//     }
+//     else if (number > generatedNumber) {
+//         console.log("If i were you, i would go even lower down to zero, at least you'd find something in common!!!");
+//    }else if (number < generatedNumber) {
+//         console.log("Pfff! Not even close... But if it pleases you go higher, it's the only way you'll get higher somewhere, in your miserable life...");
+//     } else if(number === generatedNumber) {
+//         console.log("...So, You hit the nail on the head?!! Wow! huh... I mean... BIG DEAL!!!");
+//         break;
+//     }
+
+}
 }
 
-function checkGuess(generatedNumber, playerGuess) {
 
-    if (playerGuess < generatedNumber) {
-        console.log("If i were you, i would go even lower down to zero, at least you'd find something in common!!!");
-    } else if (playerGuess > generatedNumber) {
-        console.log("Pfff! Not even close... But if it pleases you go higher, it's the only way you'll get higher somewhere, in your miserable life...");
-    } else {
-        console.log("...So, You hit the nail on the head?!! Wow! huh... I mean... BIG DEAL!!!");
-    }
-}
-
-function game() {
-    const generatedNumber = generateRandomNumber()
-    let playerGuess = getValidGuess();
-
-    checkGuess(generatedNumber, playerGuess);
+ const wait = () => new Promise(resolve => setTimeout(resolve, 0))
+ 
+async function game() {
+   
+    // const generatedNumber = generateRandomNumber()
+    // let playerGuess = getValidGuess();
     //only for degug
-    console.log(generatedNumber)
+   getPlayerGuess();
+    // console.log(generatedNumber)
 
 }
 
